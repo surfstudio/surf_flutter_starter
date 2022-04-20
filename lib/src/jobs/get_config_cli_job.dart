3a241b4c94507e732bcf5b81456926b52c675856
Ivan Taranov <ivlupsecond@gmail.com>
2022-04-20 15:03:55 +0200

    fix

--- lib/src/jobs/get_config_cli_job.dart
+++ lib/src/jobs/get_config_cli_job.dart
@@ -5,11 +5,7 @@ import 'package:surf_flutter_starter/src/repositories/config_repository.dart';
 import 'package:surf_flutter_starter/src/utils/logger.dart';
 
 /// Gets [Config] from user input.
-///
-/// Loses grip of [Job]'s interface, so that [Job.execute] would
-/// not require [Config] as an argument. It's done in that way, because
-/// this [GetConfigCLIJob] sole purpose is to get that said [Config].
-class GetConfigCLIJob {
+class GetConfigCLIJob implements Job {
   final ConfigRepository _configRepository;
 
   /// Constructor, in which repositories are passed.
