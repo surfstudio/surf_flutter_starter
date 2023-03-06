a295b9f5254b8360235b6d0f942c5bb13bdccf18
mark-kascheev <mark.kascheev@gmail.com>
2023-03-06 13:59:15 +0400

    SNP-308 feat: add test for renaming project

--- lib/test/renaming/renaming_service_test.dart
+++ lib/test/renaming/renaming_service_test.dart
@@ -1,4 +1,5 @@
 import 'package:surf_flutter_starter/src/config/config_parameter.dart';
+import 'package:surf_flutter_starter/src/constants.dart';
 import 'package:surf_flutter_starter/src/services/directory_service.dart';
 import 'package:surf_flutter_starter/src/services/renaming_service.dart';
 import 'package:surf_flutter_starter/src/utils/replace_path_separators_x.dart';
@@ -18,96 +19,164 @@ class DirectoryServiceMock extends Mock implements DirectoryService {}
 
 void main() {
   late final RenamingService renamingService;
-  final DirectoryService directoryService = DirectoryServiceMock();
+  late final DirectoryService directoryService;
 
   final mainManifestPath =
-      '$testExePath\\$projectName\\${ProjectPath.androidMainManifestPath}'.replacePathSeparators();
+      '$testExePath\\$projectName\\${ProjectPath.androidMainManifestPath}'
+          .replacePathSeparators();
   final debugManifestPath =
-      '$testExePath\\$projectName\\${ProjectPath.androidDebugManifestPath}'.replacePathSeparators();
+      '$testExePath\\$projectName\\${ProjectPath.androidDebugManifestPath}'
+          .replacePathSeparators();
   final profileManifestPath =
       '$testExePath\\$projectName\\${ProjectPath.androidProfileManifestPath}'
           .replacePathSeparators();
   final gradlePath =
-      '$testExePath\\$projectName\\${ProjectPath.androidAppGradlePath}'.replacePathSeparators();
-  final kotlinPath =
-      '$testExePath\\$projectName\\${ProjectPath.oldKotlinPath}'.replacePathSeparators();
+      '$testExePath\\$projectName\\${ProjectPath.androidAppGradlePath}'
+          .replacePathSeparators();
+  final kotlinPath = '$testExePath\\$projectName\\${ProjectPath.oldKotlinPath}'
+      .replacePathSeparators();
   final activityPath =
       '$testExePath\\$projectName\\android\\app\\src\\main\\kotlin\\com\\test\\app\\MainActivity.kt'
           .replacePathSeparators();
-  final commonPath =
-      '$testExePath\\$projectName\\${ProjectPath.iosCommonPath}'.replacePathSeparators();
+  final commonPath = '$testExePath\\$projectName\\${ProjectPath.iosCommonPath}'
+      .replacePathSeparators();
   final plistPath =
-      '$testExePath\\$projectName\\${ProjectPath.iosInfoPlistPath}'.replacePathSeparators();
+      '$testExePath\\$projectName\\${ProjectPath.iosInfoPlistPath}'
+          .replacePathSeparators();
 
   setUpAll(() {
-    /// Set up for android renaming
-    when(() => directoryService.readFileAsString(filePath: mainManifestPath))
-        .thenReturn(mainManifestTest);
-    when(() =>
-            directoryService.updateFile(filePath: mainManifestPath, content: renamedMainManifest))
-        .thenReturn(() {});
-
-    when(() => directoryService.readFileAsString(filePath: debugManifestPath))
-        .thenReturn(debugManifestTest);
-    when(() => directoryService.updateFile(
-        filePath: debugManifestPath, content: renamedDebugManifestTest)).thenReturn(() {});
-
-    when(() => directoryService.readFileAsString(filePath: profileManifestPath))
-        .thenReturn(profileManifestTest);
-    when(() => directoryService.updateFile(
-        filePath: profileManifestPath, content: renamedProfileManifestTest)).thenReturn(() {});
-
-    when(() => directoryService.readFileAsString(filePath: gradlePath)).thenReturn(gradleTest);
-    when(() => directoryService.updateFile(filePath: gradlePath, content: editedGradleTest))
-        .thenReturn(() {});
-
-    when(() => directoryService.readFileAsString(filePath: kotlinPath)).thenReturn(activity);
-    when(() => directoryService.createFile(
-        filePath: '$testExePath\\$projectName\\android\\app\\src\\main\\kotlin\\com\\test\\app'
-            .replacePathSeparators(),
-        fileName: 'MainActivity.kt',
-        isRecursive: true)).thenAnswer((_) async {});
-    when(() => directoryService.readFileAsString(filePath: activityPath)).thenReturn(activity);
-    when(() => directoryService.updateFile(filePath: activityPath, content: editedActivity))
-        .thenReturn(() {});
-    when(() => directoryService.deleteDirectory(directoryPath: kotlinPath)).thenReturn(() {});
-
-    /// Set up for IOS renaming
-    when(() => directoryService.readFileAsString(filePath: plistPath)).thenReturn(plist);
-    when(() => directoryService.updateFile(filePath: plistPath, content: editedPlist))
-        .thenReturn(() {});
-    when(() => directoryService.readFileAsString(filePath: commonPath)).thenReturn(commonFile);
-    when(() => directoryService.updateFile(filePath: commonPath, content: editedCommonFile))
-        .thenReturn(() {});
-
+    directoryService = DirectoryServiceMock();
     renamingService = RenamingService(directoryService);
   });
 
-  test('rename Android', () async {
-    await renamingService.renameAndroid(
-        executablePath: testExePath,
-        projectName: projectName,
-        appLabel: appLabel,
-        bundleId: bundleId);
+  /// Wrap [when] for reuse
+  void whenRenameFile({
+    required String path,
+    required String oldFile,
+    required String newFile,
+  }) {
+    when(() => directoryService.readFileAsString(filePath: path))
+        .thenReturn(oldFile);
+    when(() => directoryService.updateFile(
+          filePath: path,
+          content: newFile,
+        )).thenReturn(() {});
+  }
 
-    verify(() =>
-        directoryService.updateFile(filePath: mainManifestPath, content: renamedMainManifest));
+  /// Wrap [verify] for reuse
+  void verifyRenaming({required String path, required String result}) {
     verify(() => directoryService.updateFile(
-        filePath: debugManifestPath, content: renamedDebugManifestTest));
-    verify(() => directoryService.updateFile(
-        filePath: profileManifestPath, content: renamedProfileManifestTest));
-    verify(() => directoryService.updateFile(filePath: gradlePath, content: editedGradleTest));
-    verify(() => directoryService.updateFile(filePath: activityPath, content: editedActivity));
+          filePath: path,
+          content: result,
+        ));
+  }
+
+  group('renameProject', () {
+    test('should rename project directory and files', () async {
+      when(() => directoryService.updateDirectory(
+              directoryPath: '$testExePath\\$projectName'
+                  .replacePathSeparators(),
+              newName: projectName,))
+          .thenAnswer((_) {});
+      when(() => directoryService.updateAllFiles(
+              directoryPath: '$testExePath\\$projectName'
+                  .replacePathSeparators(),
+              oldValue: AppLabel.defaultAppLabel,
+              newValue: projectName,))
+          .thenAnswer((_) {});
+
+      await renamingService.renameProject(
+          executablePath: testExePath, projectName: projectName,);
+
+      verify(() => directoryService.updateDirectory(
+              directoryPath: '$testExePath\\$templateProjectNameMain'
+                  .replacePathSeparators(),
+              newName: projectName,));
+      verify(() => directoryService.updateAllFiles(
+              directoryPath: '$testExePath\\$projectName'.replacePathSeparators(),
+              oldValue: AppLabel.defaultAppLabel,
+              newValue: projectName,));
+    });
   });
 
-  test('rename IOS', () async {
-    await renamingService.renameIOS(
+  group('rename Android', () {
+    test('should rename android-specific files and contents', () async {
+      /// Rename Android
+      whenRenameFile(
+        path: mainManifestPath,
+        oldFile: mainManifestTest,
+        newFile: renamedMainManifest,
+      );
+      whenRenameFile(
+        path: debugManifestPath,
+        oldFile: debugManifestTest,
+        newFile: renamedDebugManifestTest,
+      );
+      whenRenameFile(
+        path: profileManifestPath,
+        oldFile: profileManifestTest,
+        newFile: renamedProfileManifestTest,
+      );
+      whenRenameFile(
+        path: gradlePath,
+        oldFile: gradleTest,
+        newFile: editedGradleTest,
+      );
+
+      when(() => directoryService.readFileAsString(filePath: kotlinPath))
+          .thenReturn(activity);
+      when(() => directoryService.createFile(
+            filePath:
+                '$testExePath\\$projectName\\android\\app\\src\\main\\kotlin\\com\\test\\app'
+                    .replacePathSeparators(),
+            fileName: 'MainActivity.kt',
+            isRecursive: true,
+          )).thenAnswer((_) async {});
+
+      whenRenameFile(
+        path: activityPath,
+        oldFile: activity,
+        newFile: editedActivity,
+      );
+      when(() => directoryService.deleteDirectory(directoryPath: kotlinPath))
+          .thenReturn(() {});
+
+      await renamingService.renameAndroid(
         executablePath: testExePath,
         projectName: projectName,
         appLabel: appLabel,
-        bundleId: bundleId);
+        bundleId: bundleId,
+      );
 
-    verify(() => directoryService.updateFile(filePath: plistPath, content: editedPlist));
-    verify(() => directoryService.updateFile(filePath: commonPath, content: editedCommonFile));
+      verifyRenaming(path: mainManifestPath, result: renamedMainManifest);
+      verifyRenaming(path: debugManifestPath, result: renamedDebugManifestTest);
+      verifyRenaming(
+        path: profileManifestPath,
+        result: renamedProfileManifestTest,
+      );
+      verifyRenaming(path: gradlePath, result: editedGradleTest);
+      verifyRenaming(path: activityPath, result: editedActivity);
+    });
+  });
+
+  group('rename IOS', () {
+    test('should rename ios files and contents', () async {
+      whenRenameFile(path: plistPath, oldFile: plist, newFile: editedPlist);
+      whenRenameFile(
+        path: commonPath,
+        oldFile: commonFile,
+        newFile: editedCommonFile,
+      );
+
+      await renamingService.renameIOS(
+        executablePath: testExePath,
+        projectName: projectName,
+        appLabel: appLabel,
+        bundleId: bundleId,
+      );
+
+      verifyRenaming(path: plistPath, result: editedPlist);
+      verifyRenaming(path: commonPath, result: editedCommonFile);
+    });
   });
 }
